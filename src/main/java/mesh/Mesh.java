5ac35a5cf941ee8aa3bfdbc6d381715771801a52
FabulousFox <78906517+FabulousCodingFox@users.noreply.github.com>
2022-07-09 20:29:14 +0200

    Update

--- src/main/java/mesh/Mesh.java
+++ src/main/java/mesh/Mesh.java
@@ -5,6 +5,7 @@ import static org.lwjgl.opengl.GL46.*;
 import engine.Shader;
 import texture.Texture;
 
+import java.util.Arrays;
 import java.util.Map;
 
 public class Mesh {
@@ -12,6 +13,7 @@ public class Mesh {
     private float[] mesh, indices;
     private int vbo, ibo;
     private Shader shader;
+    private VertexAttrib[] attributes;
 
     public Mesh() {}
 
@@ -19,23 +21,41 @@ public class Mesh {
         this.textures = textures;
     }
 
-    public void setMesh(float[] mesh, float[] indices, DrawMode drawMode) {
+    public void setMesh(float[] mesh, float[] indices, VertexAttrib[] vertAttrib, DrawMode drawMode) {
         if(this.vbo == 0) this.vbo = glGenBuffers();
         if(this.ibo == 0) this.ibo = glGenBuffers();
         this.mesh = mesh;
         this.indices = indices;
+        this.attributes = vertAttrib;
         int mode = drawMode== DrawMode.STATIC ? GL_STATIC_DRAW : drawMode == DrawMode.DYNAMIC ? GL_DYNAMIC_DRAW : GL_STREAM_DRAW;
         glBindBuffer(GL_ARRAY_BUFFER, this.vbo);
         glBufferData(GL_ARRAY_BUFFER, mesh, GL_STATIC_DRAW);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this.ibo);
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices, GL_STATIC_DRAW);
-
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(0);
-
         glBindBuffer(GL_ARRAY_BUFFER, 0);
-
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     }
+    public void render(Map<String, Object> uniforms){
+        shader.use();
+        shader.setUniforms(uniforms);
 
-    public void render(Map<String, Object> uniforms){}
+        int i=0;
+        for(Map.Entry<String, Texture> entry : textures.entrySet()){
+            glActiveTexture(GL_TEXTURE0 + i);
+            glBindTexture(GL_TEXTURE_2D, entry.getValue().getID());
+            shader.setInt(entry.getKey(), i);
+            i++;
+        }
+
+        glBindBuffer(GL_ARRAY_BUFFER, this.vbo);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this.ibo);
+
+        Arrays.stream(vertAttrib).forEach(VertexAttrib::apply);
+
+        glDrawElements(GL_TRIANGLES, indices.length, GL_UNSIGNED_INT, 0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+        textures.forEach((key, value) -> value.unbind());
+        shader.unbind();
+    }
 }
--- src/main/java/mesh/VertexAttrib.java
+++ src/main/java/mesh/VertexAttrib.java
@@ -1,4 +1,26 @@
 package mesh;
 
+import static org.lwjgl.opengl.GL46.*;
+
 public class VertexAttrib {
+    private int index, size, stride, offset;
+
+    public VertexAttrib(int index, int size, int stride, int offset) {
+        this.index = index;
+        this.size = size;
+        this.stride = stride;
+        this.offset = offset;
+    }
+
+    public void apply(){
+        glVertexAttribPointer(
+            index,
+            size,
+            GL_FLOAT,
+            false,
+            stride,
+            offset
+        );
+        glEnableVertexAttribArray(index);
+    }
 }
